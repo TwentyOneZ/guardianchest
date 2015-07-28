1ec81eccc0e38143fe0f54b3962859f4c71a8ca6
BuildTools <unconfigured@null.spigotmc.org>
2015-07-28 18:27:40 -0300

    Adjusting directory structure

---
+++ src/main/java/me/twentyonez/guardianchest/block/GCBlocks.java
@@ -0,0 +1,36 @@
+package me.twentyonez.guardianchest.block;
+
+import net.minecraft.block.Block;
+import me.twentyonez.guardianchest.util.ConfigHelper;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.ChatComponentText;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
+/**
+ * GuardianChest mod
+ *
+ * @author TwentyOneZ
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ * Based on NightKosh's GraveStone mod, Dr.Cyano's Lootable Corpses mod and Tyler15555's Death Chest mod.
+ */
+public class GCBlocks {
+
+	public static void mainRegistry() {
+		initialiseBlock();
+		registerBlock();
+	}
+
+	public static Block GCChest;
+	
+	private static void registerBlock() {
+		GameRegistry.registerBlock(GCChest, "GuardianChest");		
+	}
+
+	private static void initialiseBlock() {
+		// TODO Auto-generated method stub
+		GCChest = new GCChest(0).setBlockName("guardianChest").setCreativeTab(CreativeTabs.tabMisc).setBlockTextureName("guardianchest:GuardianChest").setBlockUnbreakable().setLightLevel(0.7F).setResistance(100.0F);
+	}
+	
+}
---
+++ src/main/java/me/twentyonez/guardianchest/common/GCMainRegistry.java
@@ -0,0 +1,91 @@
+package me.twentyonez.guardianchest.common;
+
+import me.twentyonez.guardianchest.block.GCBlocks;
+import me.twentyonez.guardianchest.compat.GCThaumcraft;
+import me.twentyonez.guardianchest.compat.InitCompatCheck;
+import me.twentyonez.guardianchest.item.ItemBoundMapTier0;
+import me.twentyonez.guardianchest.item.ItemBoundMapTier1;
+import me.twentyonez.guardianchest.item.ItemGuardianTier0;
+import me.twentyonez.guardianchest.item.ItemGuardianTier1;
+import me.twentyonez.guardianchest.item.ItemGuardianTier2;
+import me.twentyonez.guardianchest.util.ConfigHelper;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+
+import org.apache.logging.log4j.Level;
+
+import thaumcraft.api.ItemApi;
+import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+/**
+ * GuardianChest mod
+ *
+ * @author TwentyOneZ
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ * Based on NightKosh's GraveStone mod, Dr.Cyano's Lootable Corpses mod and Tyler15555's Death Chest mod.
+ */
+@Mod(modid = "GuardianChest", name = "Guardian Chest", version = MinecraftForge.MC_VERSION)
+public class GCMainRegistry {
+
+	@SidedProxy(clientSide = "me.twentyonez.guardianchest.common.ClientProxy", serverSide = "me.twentyonez.guardianchest.common.ServerProxy") 
+	public static ServerProxy proxy;
+	
+	public static ItemGuardianTier0 guardianTier0 = new ItemGuardianTier0();
+	public static ItemGuardianTier1 guardianTier1 = new ItemGuardianTier1();
+	public static ItemGuardianTier2 guardianTier2 = new ItemGuardianTier2();
+	public static ItemBoundMapTier0 boundMapTier0 = new ItemBoundMapTier0();
+	public static ItemBoundMapTier1 boundMapTier1 = new ItemBoundMapTier1();
+	
+	public static GCMainRegistry modInstance;
+	
+	public GCMainRegistry() {
+		
+	}
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		event.getModLog().log(Level.INFO, "Starting to load Guardian Chest!");
+		GameRegistry.registerItem(guardianTier0, "guardianTier0");
+		GameRegistry.registerItem(guardianTier1, "guardianTier1");
+		GameRegistry.registerItem(guardianTier2, "guardianTier2");
+		GameRegistry.registerItem(boundMapTier0, "boundMapTier0");
+		GameRegistry.registerItem(boundMapTier1, "boundMapTier1");
+		GCBlocks.mainRegistry();
+		proxy.registerTileEntities();
+		proxy.registerRenderThings();
+		
+		ConfigHelper.setupConfig(new Configuration(event.getSuggestedConfigurationFile()), event.getModLog());
+	}
+	
+	@EventHandler
+	public void loadMod(FMLInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(new GCEventHandler());
+		FMLCommonHandler.instance().bus().register(new GCEventHandler()); 
+		
+		GameRegistry.addRecipe(new ItemStack(GCMainRegistry.guardianTier0), "srs", "tct", "srs", 's', Blocks.stone, 'c', Blocks.chest, 'r', Items.redstone, 't', Blocks.redstone_torch);
+		GameRegistry.addShapelessRecipe(new ItemStack(GCMainRegistry.guardianTier2), GCMainRegistry.guardianTier1, GCMainRegistry.boundMapTier1);
+		GameRegistry.addShapelessRecipe(new ItemStack(GCMainRegistry.boundMapTier0), GCMainRegistry.guardianTier1, Items.map);
+		GameRegistry.addShapelessRecipe(new ItemStack(GCMainRegistry.boundMapTier1), GCMainRegistry.guardianTier2);
+	}
+	
+	@EventHandler
+	public void finishLoading(FMLPostInitializationEvent event) {
+		InitCompatCheck.getInstance().checkMods();
+	}
+
+}
